d0808ee9d255d5e5d68bef55acb42b87af4ba3d3
Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
2023-09-07 23:04:58 +0200

    Update shipofffools.py

--- pro/shipofffools.py
+++ pro/shipofffools.py
@@ -19,39 +19,148 @@ class Die:
 
 
 class DiceCup:
-    def __init__(self, die_count=1):
-        self._dice = [Die for i in range(die_count)]
-        self.locked = [True for i in range(die_count)]
-
+    def __init__(self, die_count=5):
+        self._dice = []
+        self._locked = []
+        
+        for i in range(die_count):
+            self._dice.append(Die())
+            self._locked.append(False)
 
     def roll(self):
         for i, d in enumerate(self._dice):
-            if not self.locked[i]:
+            if not self._locked[i]:
                 d.roll()
-        
-    def die_value(self, index):
-        return self._dice[index-1].get_value()
 
+    def status(self):
+        for i, d in enumerate(self._dice):
+            if not self._locked[i]:
+                print(i, d.get_value())
+            else: 
+                print(i,"(L)", d.get_value())
+
+    def die_value(self, index):
+        return self._dice[index].get_value()
 
     def bank(self, index):
-        self.locked[index-1] = True
+        self._locked[index] = True
 
-    
     def is_banked(self, index):
-        return self.locked[index-1]
-
+        return self._locked[index]
 
     def release(self, index):
-        self.locked[index-1] = False
-    
+        self._locked[index] = False
     
     def release_all(self):
-        for l in self.locked:
-            l = True
-
-
-die1 = Die()
-die2 = Die()
-print(f"{die1.get_value()} {die2.get_value()} ")
-print(f"Värdet är: {str(die1+die2)}")
+        self._locked = []
+        for i in range(len(self._dice)):
+            self._locked.append(False)
     
+    def has_value(self, value):
+        for i, die in enumerate(self._dice):
+            if die.get_value() == value:
+                self._locked[i] = True
+                return True
+
+
+class Player:
+    def __init__(self, name):
+        self._score = 0
+        self.name = name
+    
+    def current_score(self):
+        return self._score
+    
+    def get_name(self):
+        return self.name
+    
+    def reset_score(self):
+        self.score = 0
+        
+    def play_turn(self, game):
+        self._score += game.turn()
+
+
+class ShipOfFoolsGame:
+    def __init__(self):
+        self.winning_score = 50
+        self._cup = DiceCup(5)
+
+    def turn(self):
+        self._cup.release_all()
+        has_ship = False
+        has_captain = False
+        has_mate = False
+                
+        crew = 0
+        for _ in range(3):
+            self._cup.roll()
+            
+            if not has_ship and self._cup.has_value(6):
+                has_ship = True
+                
+            if has_ship and not has_captain and self._cup.has_value(5):
+                has_captain = True
+
+            if has_captain and not has_mate and self._cup.has_value(4):
+                has_mate = True
+                
+            if has_ship and has_captain and has_mate:
+                for i in range(5):
+                    self._cup.bank(i)
+
+        if has_ship and has_captain and has_mate:
+            for i in range(5):
+                crew += self._cup.die_value(i)
+            crew -= 15
+        
+        return crew
+
+class PlayRoom:
+    def __init__(self):
+        self._game = None
+        self._players  = []
+        self._winner = None
+    
+    def set_game(self, game):
+        self._game = game
+        
+    def add_player(self, player):
+        self._players.append(player)
+    
+    def reset_scores(self):
+        for player in self._players:
+            player.reset_score()
+            self._winner = None
+    
+    def play_round(self):
+        for player in self._players:
+            player.play_turn(self._game)
+        
+    def print_scores(self):
+        print("")
+        for player in self._players:
+            print("{}: {}".format(player.get_name(), player.current_score()))
+    
+    def game_finished(self):
+        for player in self._players:
+            if player.current_score() >= self._game.winning_score:
+                self._winner = player
+                return True
+        return False
+    
+    def print_winner(self):
+        print("{}: {}".format("Winner",self._winner.get_name()))
+    
+    
+if __name__ == "__main__":
+    room = PlayRoom()
+    room.set_game(ShipOfFoolsGame())
+    room.add_player(Player("Nora"))
+    room.add_player(Player("Selman"))
+    room.reset_scores()
+    room.print_scores()
+    while not room.game_finished():
+        room.play_round()
+        room.print_scores()
+    room.print_winner()
