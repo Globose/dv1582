1803f412e094b961e8015a0b5e4d909c18c24957
Tobbe821 <34543773+Tobbe821@users.noreply.github.com>
2023-10-06 00:10:36 +0200

    inl

--- sim/sim.py
+++ sim/sim.py
@@ -1,34 +1,192 @@
 from collections import deque
 from random import randrange
 
-class World:    
+class Token:
     def __init__(self):
+        pass
+    
+    
+class Worker(Token):
+    def __init__(self):
+        self._vitality = 100
+    
+    def get_vitality(self) -> int:
+        """Returns the worker's vitality"""
+        return self._vitality
+        
+    def change_vitality(self, change: int):
+        """Changes vitality by an amount"""
+        self._vitality += change
+
+        if self._vitality > 100:
+            self._vitality = 100
+        elif self._vitality < 0:
+            self._vitality = 0
+
+
+class Food(Token):
+    def __init__ (self, quality:float):
+        self._quality = quality
+
+    def get_quality(self) -> float:
+        """Returns quality of the food"""
+        return self._quality
+
+
+class Product(Token):
+    """Product"""
+
+
+class Place:
+    def __init__(self):
+        self._tokens = deque()
+    
+    def add(self, token: Token):
+        self._tokens.append(token)
+    
+    def get(self) -> Token:
+        """Returns one token, returns None if empty"""
+        if len(self._tokens) > 0:
+            return self._tokens.popleft()
+    
+    def is_empty(self) -> bool:
+        """Returns true if place is empty"""
+        return len(self._tokens) == 0
+
+
+class Barn(Place):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self) -> str:
+        return f"Barn ({len(self._tokens)})"
+
+        
+class Storage(Place):
+    def __init__(self):
+        super().__init__()
+    
+    def get(self) -> Product:
+        """Returns a product"""
+        if len(self._tokens) > 0:
+            return self._tokens.pop()
+
+    def __str__(self) -> str:
+        return f"storage ({len(self._tokens)})"
+
+
+class Barrack(Place):
+    def __init__(self):
+        super().__init__()
+    
+    def add(self, worker:Worker):
+        """Add a worker to the barrack"""
+        if worker.get_vitality() > 0:
+            self._tokens.append(worker)
+    
+    def __str__(self) -> str:
+        result = f"Barrack ({len(self._tokens)})"
+        
+        w: Worker
+        for w in self._tokens:
+            result += f"\n Worker: {w.get_vitality()}"
+        return result
+
+class Transition:
+    def __init__(self, barrack_in, barrack_out):
+        self._barrack_in = barrack_in
+        self._barrack_out = barrack_out
+        
+    def act(self):
+        pass
+
+
+class Field(Transition):
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, barn_out: Barn):
+        super().__init__(barrack_in, barrack_out)
+        self._barn_out = barn_out
+
+    def act(self):
+        """Do farming"""
+        if self._barrack_in.is_empty():
+            return
+
+        worker = self._barrack_in.get()
+        worker.change_vitality(-10)
+        self._barrack_out.add(worker)
+        self._barn_out.add(Food(1))
+
+
+class DiningHall:
+    def __init__(self, barrack_in:Barrack, barrack_out:Barrack, barn_in:Barn):
+        self._barrack_in = barrack_in
+        self._barrack_out = barrack_out
+        self._barn_in = barn_in
+        
+    def act(self):
+        """Do eating"""
+        if self._barrack_in.is_empty() or self._barn_in.is_empty():
+            return
+
+        worker = self._barrack_in.get()
+        food = self._barn_in.get()
+        worker.change_vitality(10*food.get_quality())
+        self._barrack_out.add(worker)
+
+
+class Home(Transition):
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, storage_in: Storage):
+        super().__init__(barrack_in, barrack_out)
+        self._storage_in = storage_in
+    
+    def act(self):
+        """Do resting"""
+
+
+class Factory(Transition):
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, storage_out: Storage):
+        super().__init__(barrack_in,barrack_out)
+        self._storage_out = storage_out
+    
+    def act(self):
+        """Do work"""
+        if self._barrack_in.is_empty():
+            return
+        worker = self._barrack_in.get()
+        worker.change_vitality(-10)
+        self._storage_out.add(Product())
+        self._barrack_out.add(worker)
+
+
+class World:    
+    def __init__(self, barrack_size:int, storage_size, barn_size, dining_halls_size, homes_size, fields_size, factories_size, workers_size):
         self._day = 0
-        self._barracks = []
-        self._storages = []
-        self._barns = []
-        self._dining_halls = []
-        self._homes = []
-        self._fields = []
-        self._factories = []
+        self._barracks = [Barrack() for _ in range(barrack_size)]
+        self._storage = [Storage() for _ in range(storage_size)]
+        self._barns = [Barn() for _ in range(barn_size)]
+
+        self._dining_halls = [DiningHall(self._barracks[randrange(len(self._barracks))], 
+                                         self._barracks[randrange(len(self._barracks))],
+                                         self._barns[randrange(len(self._barns))])
+                              for _ in range(dining_halls_size)]
         
-        b1 = Barrack()
-        s1 = Storage()
-        ba1 = Barn()
-        dh1 = DiningHall(b1,b1,ba1)
-        h1 = Home(b1, b1, s1)
-        f1 = Field(b1, b1, ba1)
-        fa1 = Factory(b1,b1,s1)
+        self._homes = [Home(self._barracks[randrange(len(self._barracks))], 
+                            self._barracks[randrange(len(self._barracks))], 
+                            self._storage[randrange(len(self._storage))])
+                       for _ in range(homes_size)]
         
-        for _ in range(10000):
-            b1.add_worker(Worker())
-        self._barracks.append(b1)
-        self._storages.append(s1)
-        self._barns.append(ba1)
-        self._dining_halls.append(dh1)
-        self._homes.append(h1)
-        self._fields.append(f1)
-        self._factories.append(fa1)
+        self._fields = [Field(self._barracks[randrange(len(self._barracks))],
+                              self._barracks[randrange(len(self._barracks))],
+                              self._barns[randrange(len(self._barns))])
+                        for _ in range(fields_size)]
+        
+        self._factories = [Factory(self._barracks[randrange(len(self._barracks))],
+                                   self._barracks[randrange(len(self._barracks))],
+                                   self._storage[randrange(len(self._storage))])
+                           for _ in range(factories_size)]
+        
+        for _ in range(workers_size):
+            self._barracks[randrange(len(self._barracks))].add(Worker())
 
 
     def _sim_finished(self):
@@ -47,190 +205,26 @@ class World:
             print("Day", self._day)
             f: Factory
             for f in self._factories:
-                f.work()
+                f.act()
             
             f:Field
             for f in self._fields:
-                f.farm()
+                f.act()
             
             d:DiningHall
             for d in self._dining_halls:
-                d.eat()
+                d.act()
             
             h:Home
             for h in self._homes:
-                h.rest()
+                h.act()
         
             b:Barrack
             for b in self._barracks:
-                print(b)
+                print(b)    
+    
             break
 
-
-class Worker:
-    def __init__(self):
-        self._vitality = 100
-    
-    def get_vitality(self) -> int:
-        """Returns the worker's vitality"""
-        return self._vitality
-        
-    def change_vitality(self, change: int):
-        """Changes vitality by an amount"""
-        self._vitality += change
-
-        if self._vitality > 100:
-            self._vitality = 100
-        elif self._vitality < 0:
-            self._vitality = 0
-
-
-class Food:
-    def __init__ (self, quality:float):
-        self._quality = quality
-
-    def get_quality(self) -> float:
-        """Returns quality of the food"""
-        return self._quality
-
-
-class Product:
-    """Product"""
-
-
-class Barn:
-    def __init__(self):
-        self._food = deque()
-    
-    def get_food(self) -> Food:
-        """Returns one food, returns None if empty"""
-        if len(self._food) > 0:
-            return self._food.popleft()
-        
-    def add_food(self, food:Food):
-        """Send food to barn"""
-        self._food.append(food)
-        
-    def is_empty(self) -> bool:
-        """Returns true if barn is empty"""
-        return len(self._food) == 0
-
-    def __str__(self) -> str:
-        return f"Barn ({len(self._food)})"
-
-        
-class Storage():
-    def __init__(self):
-        self._products = deque()
-    
-    def get_product(self) -> Product:
-        """Returns a product, returns none if empty"""
-        if len(self._products) > 0:
-            return self._products.pop()
-
-    def add_product(self, product:Product):
-        """Send a product to storage"""
-        self._products.append(product)
-
-    def __str__(self) -> str:
-        return f"Storage ({len(self._products)})"
-
-
-class Barrack:
-    def __init__(self):
-        self._workers = deque()
-    
-    def get_worker(self) -> Worker:
-        """Returns a worker"""
-        if len(self._workers) > 0:
-            return self._workers.popleft()
-    
-    def add_worker(self, worker:Worker):
-        """Send a worker to the barrack"""
-        if worker.get_vitality() > 0:
-            self._workers.append(worker)
-    
-    def is_empty(self) -> bool:
-        """Returns true if barrack is empty"""
-        return len(self._workers) == 0
-    
-    def __str__(self) -> str:
-        result = f"Barrack ({len(self._workers)})"
-        
-        w: Worker
-        for w in self._workers:
-            result += f"\n Worker: {w.get_vitality()}"
-        return result
-
-
-class Field:
-    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, barn_out: Barn):
-        self._barrack_in = barrack_in
-        self._barrack_out = barrack_out
-        self._barn_out = barn_out
-
-    def farm(self):
-        """Farming"""
-        print("Farming")
-        if self._barrack_in.is_empty():
-            return
-
-        print("Creating food")
-        worker = self._barrack_in.get_worker()
-        worker.change_vitality(-10)
-
-        self._barrack_out.add_worker(worker)
-        self._barn_out.add_food(Food(1))
-
-
-class DiningHall:
-    def __init__(self, barrack_in:Barrack, barrack_out:Barrack, barn_in:Barn):
-        self._barrack_in = barrack_in
-        self._barrack_out = barrack_out
-        self._barn_in = barn_in
-        
-    def eat(self):
-        """Eating"""
-        print("Eating")
-        if self._barrack_in.is_empty() or self._barn_in.is_empty():
-            return
-        print("Worker eats")
-        worker = self._barrack_in.get_worker()
-        food = self._barn_in.get_food()
-        worker.change_vitality(10*food.get_quality())
-        self._barrack_out.add_worker(worker)
-
-
-class Home:
-    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, storage_in: Storage):
-        self._barrack_in = barrack_in
-        self._barrack_out = barrack_out
-        self._storage_in = storage_in
-    
-    def rest(self):
-        """Rest"""
-        print("Resting")
-
-
-class Factory:
-    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, storage_out: Storage):
-        self._barrack_in = barrack_in
-        self._barrack_out = barrack_out
-        self._storage_out = storage_out
-    
-    def work(self):
-        """Work"""
-        print("Working")
-        if self._barrack_in.is_empty():
-            return
-        print("Worker working")
-        worker = self._barrack_in.get_worker()
-        worker.change_vitality(-10)
-        self._storage_out.add_product(Product())
-        self._barrack_out.add_worker(worker)
-
-
-    
 if __name__=='__main__':
-    w1 = World()
+    w1 = World(10, 10, 10, 10, 3, 3, 3,200)
     w1.Simulate()
---
+++ sim/sim_st.py
@@ -0,0 +1,239 @@
+from collections import deque
+from random import randrange
+
+
+class Worker:
+    def __init__(self):
+        self._vitality = 100
+
+    def get_vitality(self) -> int:
+        """Returns the worker's vitality"""
+        return self._vitality
+
+    def change_vitality(self, change: int):
+        """Changes vitality by an amount"""
+        self._vitality += change
+
+        if self._vitality > 100:
+            self._vitality = 100
+        elif self._vitality < 0:
+            self._vitality = 0
+
+
+class Food:
+    def __init__(self, quality: float):
+        self._quality = quality
+
+    def get_quality(self) -> float:
+        """Returns quality of the food"""
+        return self._quality
+
+
+class Product:
+    """Product"""
+    def __init__(self):
+        """Product constructor"""
+
+
+class Barn:
+    def __init__(self):
+        self._food = deque()
+
+    def get_food(self) -> Food:
+        """Returns one food, returns None if empty"""
+        if len(self._food) > 0:
+            return self._food.popleft()
+
+    def add_food(self, food: Food):
+        """Add food to barn"""
+        self._food.append(food)
+
+    def is_empty(self) -> bool:
+        """Returns true if barn is empty"""
+        return len(self._food) == 0
+
+    def __str__(self) -> str:
+        return f"Barn ({len(self._food)})"
+
+
+class Storage():
+    def __init__(self):
+        self._products = deque()
+
+    def get_product(self) -> Product:
+        """Returns a product, returns none if empty"""
+        if len(self._products) > 0:
+            return self._products.pop()
+
+    def add_product(self, product: Product):
+        """Add a product to storage"""
+        self._products.append(product)
+
+    def __str__(self) -> str:
+        return f"Storage ({len(self._products)})"
+
+
+class Barrack:
+    def __init__(self):
+        self._workers = deque()
+
+    def get_worker(self) -> Worker:
+        """Returns a worker"""
+        if len(self._workers) > 0:
+            return self._workers.popleft()
+
+    def add_worker(self, worker: Worker):
+        """Add a worker to the barrack"""
+        if worker.get_vitality() > 0:
+            self._workers.append(worker)
+
+    def is_empty(self) -> bool:
+        """Returns true if barrack is empty"""
+        return len(self._workers) == 0
+
+    def __len__(self) -> int:
+        """Returns number of workers in the barrack"""
+        return len(self._workers)
+
+    def __str__(self) -> str:
+        result = f"Barrack ({len(self._workers)})"
+
+        w: Worker
+        for w in self._workers:
+            result += f"\n Worker: {w.get_vitality()}"
+        return result
+
+
+class Field:
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, barn_out: Barn):
+        self._barrack_in = barrack_in
+        self._barrack_out = barrack_out
+        self._barn_out = barn_out
+
+    def farm(self):
+        """Farming"""
+        if self._barrack_in.is_empty():
+            return
+
+        worker = self._barrack_in.get_worker()
+        worker.change_vitality(-10)
+
+        self._barrack_out.add_worker(worker)
+        self._barn_out.add_food(Food(1))
+
+
+class DiningHall:
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, barn_in: Barn):
+        self._barrack_in = barrack_in
+        self._barrack_out = barrack_out
+        self._barn_in = barn_in
+
+    def eat(self):
+        """Eating"""
+        if self._barrack_in.is_empty() or self._barn_in.is_empty():
+            return
+        worker = self._barrack_in.get_worker()
+        food = self._barn_in.get_food()
+        worker.change_vitality(10*food.get_quality())
+        self._barrack_out.add_worker(worker)
+
+
+class Home:
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, storage_in: Storage):
+        self._barrack_in = barrack_in
+        self._barrack_out = barrack_out
+        self._storage_in = storage_in
+
+    def rest(self):
+        """Rest"""
+
+
+class Factory:
+    def __init__(self, barrack_in: Barrack, barrack_out: Barrack, storage_out: Storage):
+        self._barrack_in = barrack_in
+        self._barrack_out = barrack_out
+        self._storage_out = storage_out
+
+    def work(self):
+        """Work"""
+        if self._barrack_in.is_empty():
+            return
+        worker = self._barrack_in.get_worker()
+        worker.change_vitality(-40)
+        self._storage_out.add_product(Product())
+        self._barrack_out.add_worker(worker)
+
+
+class World:
+    def __init__(self, barracks_size, storages_size, barns_size,
+                 dining_halls_size, homes_size, fields_size, factories_size, workers_size):
+        self._day = 0
+        self._barracks = [Barrack() for _ in range(barracks_size)]
+        self._storages = [Storage() for _ in range(storages_size)]
+        self._barns = [Barn() for _ in range(barns_size)]
+
+        self._dining_halls = [DiningHall(self._barracks[randrange(len(self._barracks))],
+                                         self._barracks[randrange(len(self._barracks))],
+                                         self._barns[randrange(len(self._barns))])
+                              for _ in range(dining_halls_size)]
+
+        self._homes = [Home(self._barracks[randrange(len(self._barracks))],
+                            self._barracks[randrange(len(self._barracks))],
+                            self._storages[randrange(len(self._storages))])
+                       for _ in range(homes_size)]
+
+        self._fields = [Field(self._barracks[randrange(len(self._barracks))],
+                              self._barracks[randrange(len(self._barracks))],
+                              self._barns[randrange(len(self._barns))])
+                        for _ in range(fields_size)]
+
+        self._factories = [Factory(self._barracks[randrange(len(self._barracks))],
+                                   self._barracks[randrange(len(self._barracks))],
+                                   self._storages[randrange(len(self._storages))])
+                           for _ in range(factories_size)]
+
+        for _ in range(workers_size):
+            self._barracks[randrange(len(self._barracks))].add_worker(Worker())
+
+    def _is_finished(self):
+        b: Barrack
+        for b in self._barracks:
+            if not b.is_empty():
+                return False
+        return True
+
+    def Simulate(self):
+        while not self._is_finished():
+            self._day += 1
+            print()
+            print("Day", self._day)
+
+            f: Factory
+            for f in self._factories:
+                f.work()
+
+            f: Field
+            for f in self._fields:
+                f.farm()
+
+            d: DiningHall
+            for d in self._dining_halls:
+                d.eat()
+
+            h: Home
+            for h in self._homes:
+                h.rest()
+
+            workers = 0
+            b: Barrack
+            for b in self._barracks:
+                workers += len(b)
+            print(f"{workers} alive")
+
+            if self._day > 95:
+                break
+
+
+if __name__ == '__main__':
+    w1 = World(1, 10, 10, 10, 10, 10, 10, 100)
+    w1.Simulate()
